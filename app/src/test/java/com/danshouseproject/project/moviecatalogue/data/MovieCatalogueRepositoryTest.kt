151904e1e95b678d0c75c6de5ce4354912122408
movie <dandianastasa12@gmail.com>
2021-06-18 22:21:01 +0700

    add more page and do the test

---
+++ app/src/test/java/com/danshouseproject/project/moviecatalogue/data/MovieCatalogueRepositoryTest.kt
@@ -0,0 +1,32 @@
+package com.danshouseproject.project.moviecatalogue.data
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MovieCatalogueRepositoryTest {
+
+    @Test
+    fun getAllMovies() {
+    }
+
+    @Test
+    fun getAllTvShows() {
+    }
+
+    @Test
+    fun getMoviesGenres() {
+    }
+
+    @Test
+    fun getTvGenres() {
+    }
+
+    @Test
+    fun getMoviesMoreInfo() {
+    }
+
+    @Test
+    fun getTvMoreInfo() {
+    }
+}
